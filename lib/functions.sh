a191b400d5b79c5ae2c6de1ba3fbd794599101ed
Dan Johansen <strit@manjaro.org>
2019-07-04 08:37:37 +0200

    [buildarmoem] make rpi4 use sddm-n2

--- lib/functions.sh
+++ lib/functions.sh
@@ -388,7 +388,7 @@ create_rootfs_oem() {
     # Install device and editions specific packages
     mount -o bind /var/cache/manjaro-arm-tools/pkg/pkg-cache $ROOTFS_IMG/rootfs_$ARCH/var/cache/pacman/pkg
     $NSPAWN $ROOTFS_IMG/rootfs_$ARCH pacman -Syyu base $PKG_DEVICE $PKG_EDITION dialog manjaro-arm-oem-install --noconfirm
-    if [[ "$DEVICE" = "on2" ]]; then
+    if [[ "$DEVICE" = "on2" ]] || [[ "$DEVICE" = "rpi4" ]]; then
     if [[ "$EDITION" = "kde" ]] || [[ "$EDITION" = "cubocore" ]]; then
     $NSPAWN $ROOTFS_IMG/rootfs_$ARCH pacman -R sddm sddm-kcm --noconfirm
     $NSPAWN $ROOTFS_IMG/rootfs_$ARCH pacman -S sddm-n2 sddm-kcm --noconfirm
