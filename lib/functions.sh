3fd1d263642e04e7fab13e64126b13d3002a4cf5
Dan Johansen <strit@manjaro.org>
2019-07-19 15:44:28 +0200

    [buildarmoem] add xfce to list of editions to have lightdm disabled

--- lib/functions.sh
+++ lib/functions.sh
@@ -421,7 +421,7 @@ create_rootfs_oem() {
     $NSPAWN $ROOTFS_IMG/rootfs_$ARCH systemctl enable $SRV_EDITION 1> /dev/null 2>&1
     
     #disabling services depending on edition
-    if [[ "$EDITION" = "mate" ]] || [[ "$EDITION" = "mate-fta" ]] || [[ "$EDITION" = "i3" ]]; then
+    if [[ "$EDITION" = "mate" ]] || [[ "$EDITION" = "mate-fta" ]] || [[ "$EDITION" = "i3" ]] || [[ "$EDITION" = "xfce" ]]; then
     $NSPAWN $ROOTFS_IMG/rootfs_$ARCH systemctl disable lightdm.service 1> /dev/null 2>&1
     elif [[ "$EDITION" = "gnome" ]]; then
     $NSPAWN $ROOTFS_IMG/rootfs_$ARCH systemctl disable gdm.service 1> /dev/null 2>&1
