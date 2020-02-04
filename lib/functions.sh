d6d55477f014ba75fdc58b2089b00905a0e26e1f
Bernhard Landauer <bernhard@manjaro.org>
2020-02-04 18:08:52 +0100

    chown -R $SUDO_USER $PKGDIR

--- lib/functions.sh
+++ lib/functions.sh
@@ -939,7 +939,7 @@ export_and_clean() {
         info "Extracting finished package out of rootfs..."
         mkdir -p $PKGDIR/$ARCH
         cp $BUILDDIR/$ARCH/build/*.pkg.tar.xz* $PKGDIR/$ARCH/
-        chmod 666 $PKGDIR/$ARCH/$PACKAGE*
+        chown -R $SUDO_USER $PKGDIR
         msg "Package saved as {$PACKAGE} in {$PKGDIR/$ARCH}..."
         umount $BUILDDIR/$ARCH/build
 
