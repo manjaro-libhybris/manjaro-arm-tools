6ff1956aad7c89ce6a397ad734ac72c8b9b4155e
Dan Johansen <strit@manjaro.org>
2021-08-05 16:59:23 +0200

    [buildarmimg] and another fix for permissions
    Signed-off-by: Dan Johansen <strit@manjaro.org>

--- lib/functions.sh
+++ lib/functions.sh
@@ -431,8 +431,10 @@ user = "oem"' >> $ROOTFS_IMG/rootfs_$ARCH/etc/greetd/config.toml
     echo "Correcting permissions from overlay..."
     chown -R root:root $ROOTFS_IMG/rootfs_$ARCH/etc
     chown -R root:root $ROOTFS_IMG/rootfs_$ARCH/usr/{local,share}
-    if [[ "$EDITION" != "minimal" && "$EDITION" != "server" ]]; then
+    if [[ -d $ROOTFS_IMG/rootfs_$ARCH/etc/polkit-1/rules.d ]]; then
         chown root:polkitd $ROOTFS_IMG/rootfs_$ARCH/etc/polkit-1/rules.d
+    fi
+    if [[ -d $ROOTFS_IMG/rootfs_$ARCH/usr/share/polkit-1/rules.d ]]; then
         chown root:polkitd $ROOTFS_IMG/rootfs_$ARCH/usr/share/polkit-1/rules.d
     fi
     
