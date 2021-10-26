1692a224f0cfcd2f875805e546a2fe31301e1da2
Dan Johansen <strit@manjaro.org>
2021-10-26 12:21:54 +0200

    [buildarmimg] don't delete folders in /var/log/
    Signed-off-by: Dan Johansen <strit@manjaro.org>

--- lib/functions.sh
+++ lib/functions.sh
@@ -478,7 +478,7 @@ user = "oem"' >> $ROOTFS_IMG/rootfs_$ARCH/etc/greetd/config.toml
     info "Cleaning rootfs for unwanted files..."
     prune_cache
     rm $ROOTFS_IMG/rootfs_$ARCH/usr/bin/qemu-aarch64-static
-    rm -rf $ROOTFS_IMG/rootfs_$ARCH/var/log/*
+    rm -f $ROOTFS_IMG/rootfs_$ARCH/var/log/* 1> /dev/null 2>&1
     rm -rf $ROOTFS_IMG/rootfs_$ARCH/etc/*.pacnew
     rm -rf $ROOTFS_IMG/rootfs_$ARCH/usr/lib/systemd/system/systemd-firstboot.service
     rm -rf $ROOTFS_IMG/rootfs_$ARCH/etc/machine-id
