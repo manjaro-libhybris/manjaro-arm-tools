11439429e1ade8c1462159b5860066cbf0af4ad9
Bardia Moshiri <fakeshell@bardia.tech>
2023-05-15 21:17:28 -0400

    mask more services on nemomobile

--- lib/functions.sh
+++ lib/functions.sh
@@ -312,6 +312,10 @@ Server = https://mirror.bardia.tech/manjaro-libhybris/aarch64\n' $ROOTFS_IMG/roo
     if [ "$EDITION" = "nemomobile" ]; then
         msg "Masking connman"
         $NSPAWN $ROOTFS_IMG/rootfs_$ARCH systemctl mask connman
+        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH systemctl mask connman-vpn
+        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH systemctl mask connman-wait-online
+        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH systemctl mask iwd
+        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH systemctl mask ead
     fi
 
     info "Applying overlay for $EDITION edition..."
