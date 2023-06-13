b670add37f5a40082d7c4f6a3d11eef1c12398ab
Bardia Moshiri <fakeshell@bardia.tech>
2023-06-13 13:51:00 -0400

    set the correct path of extensions for gnome

--- lib/functions.sh
+++ lib/functions.sh
@@ -438,7 +438,6 @@ user = "oem"' >> $ROOTFS_IMG/rootfs_$ARCH/etc/greetd/config.toml
     ### Add extensions to make everything more mobile friendly
     if [ "$EDITION" = "gnome" ]; then
        echo "Installing gnome extensions"
-       $NSPAWN $ROOTFS_IMG/rootfs_$ARCH wget https://extensions.gnome.org/extension-data/user-themegnome-shell-extensions.gcampax.github.com.v51.shell-extension.zip
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH wget https://extensions.gnome.org/extension-data/transparent-window-movingnoobsai.github.com.v13.shell-extension.zip
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH wget https://extensions.gnome.org/extension-data/windowIsReady_Removernunofarrucagmail.com.v19.shell-extension.zip
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH wget https://extensions.gnome.org/extension-data/overview_cleanergonza.com.v4.shell-extension.zip
@@ -448,7 +447,6 @@ user = "oem"' >> $ROOTFS_IMG/rootfs_$ARCH/etc/greetd/config.toml
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH wget https://extensions.gnome.org/extension-data/desktop-cubeschneegans.github.com.v17.shell-extension.zip
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH wget https://extensions.gnome.org/extension-data/improvedosknick-shmyrev.dev.v18.shell-extension.zip
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH wget https://extensions.gnome.org/extension-data/dash-to-dockmicxgx.gmail.com.v82.shell-extension.zip
-       $NSPAWN $ROOTFS_IMG/rootfs_$ARCH gnome-extensions install --force user-themegnome-shell-extensions.gcampax.github.com.v51.shell-extension.zip
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH gnome-extensions install --force transparent-window-movingnoobsai.github.com.v13.shell-extension.zip
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH gnome-extensions install --force windowIsReady_Removernunofarrucagmail.com.v19.shell-extension.zip
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH gnome-extensions install --force overview_cleanergonza.com.v4.shell-extension.zip
@@ -459,13 +457,13 @@ user = "oem"' >> $ROOTFS_IMG/rootfs_$ARCH/etc/greetd/config.toml
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH gnome-extensions install --force improvedosknick-shmyrev.dev.v18.shell-extension.zip
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH gnome-extensions install --force dash-to-dockmicxgx.gmail.com.v82.shell-extension.zip
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH mkdir -p /usr/share/gnome-shell/extensions/
-       $NSPAWN $ROOTFS_IMG/rootfs_$ARCH mv ~/.local/share/extensions/* /usr/share/gnome-shell/extensions/
+       $NSPAWN $ROOTFS_IMG/rootfs_$ARCH mv /root/.local/share/gnome-shell/extensions/* /usr/share/gnome-shell/extensions/
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH su - manjaro -c "gsettings set org.gnome.shell disable-extension-version-validation 'true'"
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH su - manjaro -c "gsettings set org.gnome.desktop.interface text-scaling-factor 1.5"
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH su - manjaro -c "gsettings set org.gnome.desktop.interface scaling-factor 1"
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH su - manjaro -c "gsettings set org.gnome.shell enabled-extensions \"['user-theme@gnome-shell-extensions.gcampax.github.com','transparent-window-moving@noobsai.github.com','windowIsReady_Remover@nunofarruca@gmail.com','overview_cleaner@gonza.com','just-perfection-desktop@just-perfection','AlphabeticalAppGrid@stuarthayhurst','clear-top-bar@superterran.net','desktop-cube@schneegans.github.com','improvedosk@luebke.io','dash-to-dock@micxgx.gmail.com']\""
        $NSPAWN $ROOTFS_IMG/rootfs_$ARCH chmod -R 755 /usr/share/gnome-shell/extensions/
-       $NSPAWN $ROOTFS_IMG/rootfs_$ARCH rm -f *.zip
+       $NSPAWN $ROOTFS_IMG/rootfs_$ARCH rm -f /*.zip /*.tar.gz
     fi
 
     info "Correcting permissions from overlay..."
